4f751228b9e8e4b9f8bd556de2c92060d2340b9c
JP085807 <JP085807@cerner.net>
2021-02-11 14:16:02 -0600

    Create project structure

---
+++ src/main/java/com/jorgepetris/payrollrestservice/PayrollRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jorgepetris.payrollrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayrollRestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jorgepetris/payrollrestservice/PayrollRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorgepetris.payrollrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

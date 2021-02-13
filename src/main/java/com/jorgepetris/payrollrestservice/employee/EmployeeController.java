a7ed22077789286a5338c1a796597853e2746417
jpetris <jorgepetris92@gmail.com>
2021-02-12 20:08:08 -0600

    Add HATEOAS to GET methods on EmployeeController

--- src/main/java/com/jorgepetris/payrollrestservice/employee/EmployeeController.java
+++ src/main/java/com/jorgepetris/payrollrestservice/employee/EmployeeController.java
@@ -1,8 +1,14 @@
 package com.jorgepetris.payrollrestservice.employee;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -28,8 +34,18 @@ public class EmployeeController {
 
     // tag::get-aggregate-root[]
     @GetMapping("/employees")
-    public List<Employee> all() {
-        return repository.findAll();
+    CollectionModel<EntityModel<Employee>> all() {
+        List<EntityModel<Employee>> employees = repository.findAll().stream()
+        .map(employee -> EntityModel.of(
+            employee,
+            linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+            linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+        .collect(Collectors.toList());
+
+        return CollectionModel.of(
+            employees,
+            linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
+
     }
     //end::get-aggregate-root[]
 
@@ -39,9 +55,15 @@ public class EmployeeController {
     }
 
     @GetMapping("/employees/{id}")
-    Employee one(@PathVariable Long id) {
-        return repository.findById(id)
-        .orElseThrow(() -> new EmployeeNotFoundException(id));
+    EntityModel<Employee> one(@PathVariable Long id) { // EntityModel is a generic container from Spring HATEOAS that includes not only the data but a collection of links.
+
+        Employee employee = repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
+        
+        return EntityModel.of(
+            employee,
+            // linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel() asks that Spring HATEOAS build a link to the EmployeeController 's one() method, and flag it as a self link.
+            linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
+            linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
     }
 
     @PutMapping("/employees/{id}")
